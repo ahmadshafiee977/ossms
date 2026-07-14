ee9e9164f7eccfdd7be5c691ec251ce74100db16
ahmadshafiee977 <ahmadshafiee977@gmail.com>
2026-07-15 00:52:34 +0800

    servlet componen basepackage

--- src/main/java/com/ossms/Application.java
+++ src/main/java/com/ossms/Application.java
@@ -5,7 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
-@ServletComponentScan
+@ServletComponentScan(basePackages = { "staff.controller", "department.controller", "item.controller",
+		"request.controller", "inheritance.controller" })
 public class Application {
 
 	public static void main(String[] args) {
